7878404ce5d8868ad32bf0b361b89ad57c0551a6
Nayana <nayana@corp-gems.com>
2018-08-23 02:45:23 +0700

    m

--- public/js/entrance.js
+++ public/js/entrance.js
@@ -414,14 +414,3 @@ var initDb = function() {
 };
 
 
-/ success callback when requesting audio input stream
-function gotStream(stream) {
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    var audioContext = new AudioContext();
-
-    // Create an AudioNode from the stream.
-    var mediaStreamSource = audioContext.createMediaStreamSource( stream );
-
-    // Connect it to the destination to hear yourself (or any other node for processing!)
-    mediaStreamSource.connect( audioContext.destination );
-}
